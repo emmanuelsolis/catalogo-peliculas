cf718ddd83f70e780be2c7dee82170812ee9e831
Emmanuel Solis <emma.ruso@outlook.com>
2023-10-26 23:11:07 -0600

    agregamos el footer component responsivo

--- catalogo_de_peliculas/src/app/app-routing.module.ts
+++ catalogo_de_peliculas/src/app/app-routing.module.ts
@@ -1,33 +1,30 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 import { ListMovieComponent } from './Pages/list-movie/list-movie.component';
 import { DetailsMovieComponent } from './Pages/details-movie/details-movie.component';
 import { HomePageComponent } from './Pages/home-page/home-page.component';
-import { NavbarComponent } from './Pages/Partials/navbar/navbar.component';
 import { FooterComponent } from './Pages/Partials/footer/footer.component';
 
+
 const routes: Routes = [
-  {path: '', component: HomePageComponent, title: 'Pagina de Inicio'},
-  {path: 'movies', component: ListMovieComponent, title: 'Listado de Peliculas'},
-  {path: 'movies/:id', component: DetailsMovieComponent, title: 'Detalles de Pelicula'}
+  { path: '', component: HomePageComponent, title: 'Pagina de Inicio' },
+  { path: 'movies', component: ListMovieComponent, title: 'Listado de Peliculas' },
+  { path: 'movies/:id', component: DetailsMovieComponent, title: 'Detalles de Pelicula' }
 ]
+
 @NgModule({
   declarations: [
-    AppComponent,
     ListMovieComponent,
     DetailsMovieComponent,
     HomePageComponent,
-    NavbarComponent, 
     FooterComponent
+   
   ],
   imports: [
-    BrowserModule, RouterModule.forRoot(routes), HttpClientModule
+    RouterModule, RouterModule.forRoot(routes), BrowserModule, HttpClientModule
   ],
-  exports: [RouterModule],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [RouterModule]
 })
-export class AppModule { }
+export class AppRoutingModule { }
